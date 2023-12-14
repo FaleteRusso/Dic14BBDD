99f60982dbf794f34e0726c9a10413516715d30a
FaleteRusso <faleterusso89@gmail.com>
2023-12-14 19:50:50 +0100

    Initial commit

--- src/com/corenetworks/presentacion/ProbarSocios.java
+++ src/com/corenetworks/presentacion/ProbarSocios.java
@@ -1,2 +1,22 @@
-package com.corenetworks.presentacion;public class ProbarSocios {
+package com.corenetworks.presentacion;
+
+import com.corenetworks.modelo.Socio;
+import com.corenetworks.persistencia.AccesoSocios;
+
+import java.sql.SQLException;
+
+public class ProbarSocios {
+    public static void main(String[] args) {
+        AccesoSocios as1 =new AccesoSocios();
+        Socio s = new Socio("1234567B","213452");
+
+            try {
+                System.out.println("Se ha inseratado socio? "+ as1.alta(s));
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            } catch (ClassNotFoundException e) {
+                System.out.println(e.toString());
+            }
+
+    }
 }
